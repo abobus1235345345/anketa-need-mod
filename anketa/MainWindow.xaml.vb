5776903f70009e8e95e046534fcce357c527d57b
abobus1235345345 <139453779+abobus1235345345@users.noreply.github.com>
2024-04-05 13:52:31 +0300

    Add files via upload

---
+++ anketa/MainWindow.xaml.vb
@@ -0,0 +1,19 @@
+﻿Class MainWindow
+
+    Private Sub CheckBox_Checked(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub CheckBox_Checked_1(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        MessageBox.Show("спасибо что прошли опрос,ожидайте результатов(поставьте 5 пожалуйста)!")
+        Close()
+    End Sub
+
+    Private Sub RadioButton_Checked(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
